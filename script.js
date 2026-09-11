31b111638df8df20443b0571c07fe1585dcaf82d
Upender Arukonda <109522168+UArukonda@users.noreply.github.com>
2026-09-11 15:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const sizeInfo = document.querySelector("#sizeInfo h1");
+
+function updateSize() {
+  sizeInfo.innerText =
+    `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
+
+updateSize();
+
+window.addEventListener("resize", updateSize);
